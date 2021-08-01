e3d1be84486886ab182714ca11b03c4d548d5257
yusuke ohta <youohta7755@gmail.com>
2021-08-01 18:02:20 +0900

    deploy.rb updated

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.16.0'
 # デプロイするアプリケーション名
-set :application, 'ar'
+set :application, 'achieve'
 # cloneするgitのレポジトリ
 # （xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
 set :repo_url, 'git@github.com:yusuke7755/achieve.git'
